99e1842e29123993f41605ec4ceb3c146bb51928
Esraa Emad <esraa.emad017@gmail.com>
2020-12-25 13:31:28 +0200

    Added the project.

---
+++ Kido Learning/Numbers category/j.js
@@ -0,0 +1,13 @@
+function changeBodyBg(pic){
+var image = 'url(' + pic + '.jpg)';
+localStorage.setItem("myimage",image);
+var a=localStorage.getItem("myimage");
+document.body.style.backgroundImage= a;
+    }
+
+function changeBackground()
+{
+var a=localStorage.getItem("myimage");
+document.body.style.background= a;
+}
+
